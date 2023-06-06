efdaf76f7b48b02aaad88f76360c50fd223cef60
jhonatanCastor <jhonatanjhonatan907@gmail.com>
2023-06-06 16:36:09 -0300

    featureUserPassword: UserToken Router

--- src/modules/users/userToken/router/passwordUser.ts
+++ src/modules/users/userToken/router/passwordUser.ts
@@ -1,9 +1,11 @@
 import { Router } from "express";
 import {celebrate, Joi, Segments} from 'celebrate';
 import ForgotPasswordController from "../controller/FortgotPasswordController";
+import ResetPasswordController from "../controller/ResetPasswordController";
 
 const passwordRouter = Router();
 const forgotPasswordControllerr = new ForgotPasswordController();
+const resetPasswordController = new ResetPasswordController()
 
 passwordRouter.post('/forgot',
 celebrate({
@@ -12,6 +14,17 @@ celebrate({
   },
 }),
 forgotPasswordControllerr.create,
+);
+
+passwordRouter.post('/reset',
+celebrate({
+  [Segments.BODY]: {
+    token: Joi.string().uuid().required(),
+    password: Joi.string().required(),
+    password_confirmation: Joi.string().required().valid(Joi.ref('password')),
+  },
+}),
+resetPasswordController.create,
 );
 
 export default passwordRouter;
